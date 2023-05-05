e9abf830bc06cc1f588f75ef5b17240fb0b5d370
jvasquez-2021117 <jvasquez-2021117@kinal.edu.gt>
2023-05-05 11:15:48 -0600

    Room type

--- GestorHoteles (Back-end)/configs/app.js
+++ GestorHoteles (Back-end)/configs/app.js
@@ -8,11 +8,15 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3200;
 
+const roomTypeRoutes = require('../src/roomType/roomType.routes');
+
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
+
+app.use('/roomType', roomTypeRoutes);
 
 exports.initServer = () =>{
     app.listen(port);
