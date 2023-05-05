8d00c0d19d4395f5d8f9a0385925daa93f5126bf
jvasquez-2021117 <jvasquez-2021117@kinal.edu.gt>
2023-05-05 11:50:36 -0600

    Room

--- GestorHoteles (Back-end)/src/room/room.controller.js
+++ GestorHoteles (Back-end)/src/room/room.controller.js
@@ -1,1 +1,21 @@
 'use strict'
+
+const Room = require('./room.model');
+
+exports.test = (req, res)=>{
+    return  res.send({message: 'Test fuction is running'});
+}
+
+exports.add = async(req, res)=>{
+    try{
+        let data = req.body;
+        let roomExists = await Room.findOne({name: data.name});
+        if(roomExists) return res.send({message: 'Room already exists'});
+        let newRoom = new Room(data);
+        await newRoom.save();
+        return res.status(200).send({message: 'Room created successfully'});
+    }catch(e){
+        console.error(e);
+        return res.status(500).send({message: 'Error adding'});
+    }
+}
