f3b70c9c513d7f8a62814eb1fe6b5923f61e54a4
jvasquez-2021117 <jvasquez-2021117@kinal.edu.gt>
2023-05-08 09:28:29 -0600

    RoomType otra vez y Room

--- GestorHoteles (Back-end)/src/room/room.routes.js
+++ GestorHoteles (Back-end)/src/room/room.routes.js
@@ -5,5 +5,11 @@ const api = express.Router();
 const roomController = require('./room.controller');
 
 api.get('/', roomController.test);
+api.post('/add', roomController.add);
+api.put('/update/:id', roomController.update);
+api.delete('/delete/:id', roomController.delete);
+api.get('/get', roomController.get);
+api.get('/getById/:id', roomController.getById);
+
 
 module.exports = api;
--- GestorHoteles (Back-end)/src/roomType/roomType.model.js
+++ GestorHoteles (Back-end)/src/roomType/roomType.model.js
@@ -3,8 +3,9 @@
 const mongoose = require('mongoose');
 
 const roomTypeSchema = mongoose.Schema({
-    description: {
-        type: String
+    name: {
+        type: String,
+        required: true
     }
 }, {
     versionKey: false
